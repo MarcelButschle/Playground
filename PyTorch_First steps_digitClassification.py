8dcb3f58bd6ace8a69fa478b18d506fec31fba94
MarcelButschle <67736746+MarcelButschle@users.noreply.github.com>
2020-07-09 17:22:52 +0200

    newbranch

---
+++ PyTorch_First steps_digitClassification.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 17:20:48 2020
+
+@author: Marcel
+"""
+
---
+++ PyTorch_First steps_regression.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 17:21:21 2020
+
+@author: Marcel
+"""
+
